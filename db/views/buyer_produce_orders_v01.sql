e091418568a9d08ed28c2cc6b9fa979d178bb21d
Chase Blokker <chaseblokker@yahoo.com>
2024-05-05 21:58:13 -0700

    remove typo in buyer_produce_orders table view

--- db/views/buyer_produce_orders_v01.sql
+++ db/views/buyer_produce_orders_v01.sql
@@ -16,23 +16,3 @@ WHERE
   o.cancelled_at IS NULL
   AND
   ii.quantity > 0;
-
-
-SELECT
-  o.buyer_id,
-  ii.produce_id,
-  date_trunc('day', od.created_at) AS order_date,
-  LEAD(date_trunc('day', od.created_at)) OVER (
-    PARTITION BY od.buyer_id, ii.produce_id
-    ORDER BY date_trunc('day', od.created_at)
-  ) AS next_order_date
-FROM
-  order_details od
-JOIN
-  order o ON o.id = od.order_id
-JOIN
-  inventory_items ii ON ii.id = od.inventory_item_id
-WHERE
-  o.cancelled_at IS NULL
-  AND
-  ii.quantity > 0;
